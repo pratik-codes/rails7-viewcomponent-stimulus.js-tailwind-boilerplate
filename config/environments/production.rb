7e1f57258119d8d72e6cfa4243ea67d3dc6f324f
pratik-codes <velocity@ip-192-168-1-4.ec2.internal>
2022-11-16 21:32:51 +0530

    changing config hostname on prod and dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "web-production-b85f.up.railway.app"
+  config.hosts << "web-production-9c09.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
